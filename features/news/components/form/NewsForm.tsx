9321f38e340e596905caf0bc73f4d418efc163c5
mohsen-ramshini <rsmohsen20@gmail.com>
2025-05-13 12:58:48 +0330

    build error fixed

--- features/news/components/form/NewsForm.tsx
+++ features/news/components/form/NewsForm.tsx
@@ -1,7 +1,4 @@
 import React, { useEffect, useState } from "react";
-import DatePicker from "react-multi-date-picker";
-import persian from "react-date-object/calendars/persian";
-import persian_fa from "react-date-object/locales/persian_fa";
 
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
